0a080f451ca87e0f7dcfdcf07d857cd557476c9c
Kateryna Rieznik <rieznik.k@gmail.com>
2023-01-30 14:30:41 +0200

    catalog mobile

--- webpack.config.js
+++ webpack.config.js
@@ -25,8 +25,16 @@ module.exports = {
               },
             },
           },
+          {
+            loader: 'resolve-url-loader'
+          },
           // Compiles Sass to CSS
-          "sass-loader"
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true, // important for resolve-url-loader
+            }
+          }
         ]
       }
     ],
